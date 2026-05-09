37344f753a901510a64217af1b361a3cba194c12
Daksh Singh <daksh.samdhyan05@gmail.com>
2026-05-09 13:00:12 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        Map<Character, Integer> str1 = new HashMap<>();
+        Map<Character, Integer> str2 = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            str1.put(s.charAt(i), str1.getOrDefault(s.charAt(i), 0) + 1);
+            str2.put(t.charAt(i), str2.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return str1.equals(str2);
+    }
+}
