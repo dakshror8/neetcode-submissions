6ec1dd8cb0671026dd1db035a3fc53a3fc7d8d67
Daksh Singh <daksh.samdhyan05@gmail.com>
2026-08-07 18:08:19 +0530

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(t == "") return "";
+        int[] res = {-1,-1};
+        int resLen = Integer.MAX_VALUE;
+        
+        Map<Character, Integer> countT = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+
+        for(char c : t.toCharArray()){
+            countT.put(c, countT.getOrDefault(c, 0) +1);
+        }
+        int need = countT.size();
+        int have = 0;
+        int l = 0;
+        for(int r=0; r<s.length(); r++){
+            char curr = s.charAt(r);
+            window.put(curr, window.getOrDefault(curr, 0) +1);
+            if(countT.containsKey(curr) && window.get(curr).equals(countT.get(curr))){
+                have++;
+            }
+            while(have == need){
+                if(r-l+1 < resLen){
+                    resLen = r-l+1;
+                    res[0] = l;
+                    res[1] = r;
+                }
+                char left = s.charAt(l);
+                window.put(left, window.get(left)-1);
+                if(countT.containsKey(left) && window.get(left) < countT.get(left)){
+                    have--;
+                }
+                l++;
+            }
+            
+        }
+        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1]+1);
+    }
+}
