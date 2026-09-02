f820afcebc6570c4f91faa82096c5c67289e1b10
Daksh Singh <daksh.samdhyan05@gmail.com>
2026-09-02 10:28:50 +0530

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        dp = [[-1] * n for _ in range(0, m)]
+
+        def dfs(i, j):
+            if i >= m or j >= n:
+                return 0
+            if dp[i][j] != -1:
+                return dp[i][j]
+
+            if text1[i] == text2[j]:
+                dp[i][j] = 1 + dfs(i+1, j+1)
+            else:
+                dp[i][j] = max(dfs(i+1, j), dfs(i, j+1))
+
+            return dp[i][j]
+
+        return dfs(0,0)
