60d58dc58456c0465a33b68a2a3d927d77cbf268
Daksh Singh <daksh.samdhyan05@gmail.com>
2026-09-07 10:22:30 +0530

    Add: edit-distance - submission-2

---
+++ Data Structures & Algorithms/edit-distance/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n, m = len(word1), len(word2)
+        dp = {}
+
+        def dfs(i, j):
+            if j == m:
+                return n-i
+            if i == n:
+                return m-j
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if word1[i] == word2[j]:
+                dp[(i, j)] = dfs(i+1, j+1)
+                return dp[(i, j)]
+
+            res = min(dfs(i, j+1), dfs(i+1, j))
+            res = min(res, dfs(i+1, j+1))
+            dp[(i, j)] = res+1
+            return res + 1
+        
+        return dfs(0,0)
